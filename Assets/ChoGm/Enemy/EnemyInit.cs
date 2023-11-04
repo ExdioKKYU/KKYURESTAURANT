bfb33faca9e5a88c70622f24b0e4e9ecc85190d0
ChoGM <148638359+ChoGM@users.noreply.github.com>
2023-11-05 00:22:51 +0900

    231105
    움직이고 사거리 안에 적이들어오면 멈춤 / x,y구분없어서 x축으로만 구현 변경해야함

--- Assets/ChoGm/Enemy/EnemyInit.cs
+++ Assets/ChoGm/Enemy/EnemyInit.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitInit : MonoBehaviour
+public class EnemyInit : MonoBehaviour
 {
-    private float moveSpeed;
-    private float rangeUnit;   //유닛의 사거리에 따른 스크립트 받아오기
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 4.0f;
-        rangeUnit = 6.5f;      //유닛의 사거리에 따른 스크립트 받아오기
+        
     }
 
     // Update is called once per frame
--- Assets/ChoGm/UnitMove.cs
+++ Assets/ChoGm/UnitMove.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class UnitMove : MonoBehaviour
 {
+    private float moveSpeed;
     private float speed;
-    private bool closeEnemy;    
-    private float rangeEnemy;   //유닛의 사거리에 따른 스크립트 받아오기
+    private float rangeUnit;   //유닛의 사거리에 따른 스크립트 받아오기
     // Start is called before the first frame update
     void Start()
     {
-        speed = 3.0f;
-        rangeEnemy = 5.0f;      //유닛의 사거리에 따른 스크립트 받아오기
-        closeEnemy = true;
+        moveSpeed = 4.0f;
+        speed = 0;
+        rangeUnit = 6.5f;      //유닛의 사거리에 따른 스크립트 받아오기
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(closeEnemy == true)
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+        // EnemyInit를 가지고 있는 모든 오브젝트를 찾음
+        EnemyInit floatCloseEnemy = GameObject.FindObjectOfType <EnemyInit> ();
 
-        if (closeEnemy == false)
-            transform.Translate(0, 0, 0);
+        //EnemyInit를 가지고 있는 오브젝트와의 거리를 계산
+        float closeDistance = Vector3.Distance(transform.position, floatCloseEnemy.transform.position);
+
+        //EnemyInit를 가지고 있는 오브젝트와의 거리와 나의 사거리를 비교해 이동
+        if (closeDistance <= rangeUnit)
+        {
+            speed = 0;
+        }
+        if (closeDistance >= rangeUnit)
+        {
+            speed = moveSpeed;
+        }
+        transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 }
