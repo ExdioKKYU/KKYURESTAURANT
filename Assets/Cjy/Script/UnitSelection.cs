dfc219acf2f91631e69a6c45995876ef175cd2cc
Jiyeong0101 <choejiyeong564@gmail.com>
2024-01-04 22:40:36 +0900

    1/4 유닛선택
    코드 다시 한번 보고 유닛 선택 코드 짜보았으나 오류 있어 다시 작성해야함

--- Assets/Cjy/Script/UnitSelection.cs
+++ Assets/Cjy/Script/UnitSelection.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class UnitSelection : MonoBehaviour
 {
-    // 버튼에 연결된 유닛을 저장할 배열 또는 리스트
-    public GameObject[] unitButtons;
+    public GameObject unitPrefab; // 소환할 유닛 프리팹
+    public KeyCode summonKey = KeyCode.S; // 소환할 키
+    public Transform spawnArea; // 소환 영역
+    public Vector2 fixedSpawnPosition; // 고정된 소환 위치
 
-    void Start()
+    void Update()
     {
+        // 특정 키를 눌렀을 때 유닛 소환
+        if (Input.GetKeyDown(summonKey))
+        {
+            SummonUnit();
+        }
     }
 
-    void SelectUnit(int unitIndex)
+    void SummonUnit()
     {
-        // 여기에서 선택된 유닛에 대한 추가 처리를 수행하면 됩니다.
-        GameObject selectedUnit = unitButtons[unitIndex];
+        // 선택된 오브젝트의 좌표를 기준으로 소환 위치를 조정
+        Vector3 spawnPosition = spawnArea.position;
 
-        Debug.Log("Selected Unit: " + selectedUnit.name);
+        // 고정된 소환 위치를 사용
+        spawnPosition += new Vector3(fixedSpawnPosition.x, fixedSpawnPosition.y, 0f);
+
+        Instantiate(unitPrefab, spawnPosition, Quaternion.identity);
     }
 }
