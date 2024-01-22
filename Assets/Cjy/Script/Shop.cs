20d5e72130e70e10fee1ceb717f5739210fb358d
Jiyeong0101 <choejiyeong564@gmail.com>
2024-01-22 23:15:42 +0900

    240122
    유닛별 코인 차감

--- Assets/Cjy/Script/Shop.cs
+++ Assets/Cjy/Script/Shop.cs
@@ -7,14 +7,16 @@ using UnityEditor.VersionControl;
 
 public class Shop : MonoBehaviour
 {
-    public float CoinInt ; // 코인이 저장되는 변수
+    public float CoinInt; // 코인이 저장되는 변수
     public Text CoinText; //코인을 표시할 오브젝트
     public float Timer; //타이머
     private bool TimeSet; // 타이머 작동여부
+    public float UnitCoin; //유닛별 코인 
 
     void Start()
     {
         TimeSet = true;
+        UnitCoin = 0;
     }
 
     void Update()
@@ -28,7 +30,7 @@ public class Shop : MonoBehaviour
             {
                 Timer = 0;
                 CoinInt += 100f;
-                
+
                 if (CoinInt >= 2000f)
                 {
                     TimeSet = false;
@@ -40,10 +42,10 @@ public class Shop : MonoBehaviour
 
     public void lostMoney() // 돈을 잃음
     {
-        if (CoinInt >= 100f) // CoinInt가 100이상이라면
+        if (CoinInt >= UnitCoin) // CoinInt가 유닛코인 이상이라면
         {
-            Debug.Log("100원 차감");
-            CoinInt -= 100f; // CoinInt가 100 줌
+            Debug.Log("차감" + UnitCoin);
+            CoinInt -= UnitCoin; // CoinInt가 유닛 코인만큼 줌
             TimeSet = true;
 
         }
