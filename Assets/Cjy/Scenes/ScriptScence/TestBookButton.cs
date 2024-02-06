cddf19b07b2deeb97d0e229c643e35e1b0850ac3
Jiyeong0101 <choejiyeong564@gmail.com>
2024-02-06 23:06:34 +0900

    240206
    도감 완성

--- Assets/Cjy/Scenes/ScriptScence/TestBookButton.cs
+++ Assets/Cjy/Scenes/ScriptScence/TestBookButton.cs
@@ -4,30 +4,58 @@ using UnityEngine;
 
 public class TestBookButton : MonoBehaviour
 {
-    public GameObject image1;
-    public GameObject image2;
-
-    private bool isImage1Active = true;
+    public GameObject[] images;
+    private int currentIndex = 0;
 
     void Start()
     {
-        image1.SetActive(true);
-        image2.SetActive(false);
+        // 첫 번째 이미지만 활성화
+        ActivateImage(currentIndex);
     }
 
-    public void OnButtonClick()
+    public void OnNextButtonClick()
     {
-        if (isImage1Active)
+        // 다음 이미지로 이동
+        if (currentIndex < images.Length - 1)
         {
-            image1.SetActive(false);
-            image2.SetActive(true);
-            isImage1Active = !isImage1Active;
-         }
+            currentIndex++;
+        }
         else
         {
-            image1.SetActive(true);
-            image2.SetActive(false);
-            isImage1Active = !isImage1Active;
+            Debug.Log("마지막 이미지입니다.");
+            return;
+        }
+        ActivateImage(currentIndex);
+    }
+
+    public void OnPreviousButtonClick()
+    {
+        // 이전 이미지로 이동
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+        }
+        else
+        {
+            Debug.Log("첫 번째 이미지입니다.");
+            return;
+        }
+        ActivateImage(currentIndex);
+    }
+
+    private void ActivateImage(int index)
+    {
+        // 현재 인덱스의 이미지만 활성화
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (i == index)
+            {
+                images[i].SetActive(true);
+            }
+            else
+            {
+                images[i].SetActive(false);
+            }
         }
     }
 }
