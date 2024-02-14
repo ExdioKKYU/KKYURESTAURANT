340b3a5810aa9ef8e2a36f5e6cd6dee119b96c24
ChoGM <148638359+ChoGM@users.noreply.github.com>
2024-02-15 00:03:14 +0900

    Revert "240214"
    This reverts commit 5184a00aead3187c74fdb158b4809e19e4819d8f.

--- Assets/Cjy/Script/Shop.cs
+++ Assets/Cjy/Script/Shop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; //추가
 using UnityEngine.SceneManagement; //추가
+using UnityEditor.VersionControl;
 
 public class Shop : MonoBehaviour
 {
